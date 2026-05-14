067d9e51ba9c3a7f47ae6535452b8b2c866469b5
Dima <dimatop930@gmail.com>
2026-05-15 01:07:42 +0500

    final bothost fix

--- bootstrap-firebase-admin.js
+++ bootstrap-firebase-admin.js
@@ -3,6 +3,8 @@ import fs from "fs";
 const out = "./firebase-admin.json";
 const b64 = process.env.FIREBASE_ADMIN_BASE64;
 
+console.log("BOOTSTRAP_FIREBASE_START");
+
 if (!fs.existsSync(out) && b64) {
   fs.writeFileSync(out, Buffer.from(b64, "base64").toString("utf8"));
   console.log("firebase-admin.json created from ENV");
