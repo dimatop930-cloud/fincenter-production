1015db66f863a783885f28a6c29553cfe35de167
Dima <dimatop930@gmail.com>
2026-05-15 00:58:06 +0500

    create firebase admin from env

---
+++ bootstrap-firebase-admin.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+const out = "./firebase-admin.json";
+const b64 = process.env.FIREBASE_ADMIN_BASE64;
+
+if (!fs.existsSync(out) && b64) {
+  fs.writeFileSync(out, Buffer.from(b64, "base64").toString("utf8"));
+  console.log("firebase-admin.json created from ENV");
+} else if (fs.existsSync(out)) {
+  console.log("firebase-admin.json already exists");
+} else {
+  console.log("FIREBASE_ADMIN_BASE64 is not set");
+}
